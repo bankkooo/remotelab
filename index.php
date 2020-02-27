7a9c73cfac7a673eff485732120222a980cfe027
nattapongpk <nattapong-ppk@hotmail.com>
2020-02-27 21:43:45 +0700

    add remote file

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<?php include("mqtt_test.php") ?>
-<html>
-<head>
-</head>
-<body>
- <p id="demo">HELLO WORLD.</p>
-</body>
-</html>
--- sendsuccess.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $cmdselected = $_POST["cmd"];
-    echo "<script language=\"JavaScript\">";
-    echo "alert('Sawatdee : $cmdselected');";
-    echo "</script>";
-?>
